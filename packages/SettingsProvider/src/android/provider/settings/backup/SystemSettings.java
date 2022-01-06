e623fb2841f4136a1a229f6935a39ca66288fee3
Ido Ben-Hur <idoybh2@gmail.com>
2022-01-06 20:45:48 +0200

    BatteryMeterView: Allow disabling QS battery estimates [1/2]
    Adapted to Sv2
    
    Change-Id: I0a14124d7f97ea12a98ab0a8feed06508f180c6b
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -103,5 +103,6 @@ public class SystemSettings {
         Settings.System.CAMERA_FLASH_NOTIFICATION,
         Settings.System.SCREEN_FLASH_NOTIFICATION,
         Settings.System.SCREEN_FLASH_NOTIFICATION_COLOR,
+        Settings.System.QS_SHOW_BATTERY_ESTIMATE,
     };
 }
